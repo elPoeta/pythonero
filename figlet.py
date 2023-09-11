88d559d4ff7d9b97e544ddf668e94fa689b53568
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2023-09-11 08:10:32 -0300

    refactor figlet

--- figlet.py
+++ figlet.py
@@ -1,21 +1,39 @@
 import sys
+import random
 from pyfiglet import Figlet
 
-if len(sys.argv) < 3 or len(sys.argv)  >= 4:
-  sys.exit("Invalid usage")
+def main():
+  length = len(sys.argv)
+  if length <= 2:
+    sys.exit("Invalid usage")
+  elif length > 2:
+    if len(sys.argv) < 3 or len(sys.argv)  >= 4:
+      sys.exit("Invalid usage")
 
-flag, font = sys.argv[1:]
+    flag, font = sys.argv[1:]
 
-if flag != "-f":
-   sys.exit("Invalid usage")
+    if flag != "-f" and flag != "-font":
+      sys.exit("Invalid usage")
 
-figlet = Figlet()
+    figlet = Figlet()
+    fonts = figlet.getFonts()
 
-fonts = figlet.getFonts()
+    if font not in fonts:
+      sys.exit("Invalid usage")
 
+    write_output(figlet, font)
 
-figlet.setFont(font=font)
+  else:
+    figlet = Figlet()
+    fonts = figlet.getFonts()
+    random_font = random.choice(fonts)
+    write_output(figlet, random_font)
 
-s = input("Input: ")
+def write_output(figlet, font):
+    figlet.setFont(font=font)
+    s = input("Input: ")
+    print("Output:")
+    print(figlet.renderText(s))
 
-print(figlet.renderText(s))
+if __name__ == "__main__":
+  main()
