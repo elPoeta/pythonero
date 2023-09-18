ca2912fffbace44c61dae061066fbada06f4da37
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2023-09-18 09:22:39 -0300

    fix regex add test

--- regular_expressions/test_numb3rs.py
+++ regular_expressions/test_numb3rs.py
@@ -6,3 +6,6 @@ def test_validate():
   assert validate("512.512.512.512") == False
   assert validate("3.2.1.1000") == False
   assert validate("cat") == False
+  assert validate("1.2.3.4") == True
+  assert validate("11.99.22.88") == True
+  assert validate("198.777.298.862") == False
