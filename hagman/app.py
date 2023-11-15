31723130478120601ef97e1b7c92f04e991d4ac5
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2023-11-15 09:05:04 -0300

    create hagman props, draw hagman pics

--- hagman/app.py
+++ hagman/app.py
@@ -1,13 +1,15 @@
 import os
 import time
-
+from hagman import Hagman
 
 def main():
     print(os.name)
     print('test clear ')
-    time.sleep(3)
+    time.sleep(1)
     os.system('clear' if os.name == 'posix' else 'cls')
+    hagman = Hagman()
+    hagman.print_pics(0)
     
 
-if __name__ == 'main':
+if __name__ == '__main__':
     main()   
--- hagman/hagman.py
+++ hagman/hagman.py
@@ -1,2 +1,17 @@
 class Hagman:
-    ...
+    def __init__(self, word="hello"):
+        self.word = word.upper()
+        self.fails = 0
+        self.display = "_ " * len(self.word)
+        self.limit_fail = 5;
+        self.ascii_pics = [
+             "   _____   \n  |     |  \n  |     |  \n  |        \n  |        \n  |        \n  |        \n__|__      \n",
+             "   _____   \n  |     |  \n  |     |  \n  |     |  \n  |        \n  |        \n  |        \n__|__      \n",
+             "   _____   \n  |     |  \n  |     |  \n  |     |  \n  |     O  \n  |        \n  |        \n__|__      \n",
+             "   _____   \n  |     |  \n  |     |  \n  |     |  \n  |     O  \n  |     |  \n  |        \n__|__      \n",
+             "   _____   \n  |     |  \n  |     |  \n  |     |  \n  |     O  \n  |    /|\ \n  |        \n__|__      \n",
+             "   _____   \n  |     |  \n  |     |  \n  |     |  \n  |     O  \n  |    /|\ \n  |    / \ \n__|__      \n",
+        ]
+    
+    def print_pics(self, pos=0):
+        print(self.ascii_pics[pos])    
