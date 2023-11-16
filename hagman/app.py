06df9e9c73a8d2f99b5054e5905ed07aaae6dd49
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2023-11-16 08:38:56 -0300

    read list of words

--- hagman/app.py
+++ hagman/app.py
@@ -1,14 +1,24 @@
-import os
-import time
+import sys
 from utils import clear_screen, get_args
+from game import Game
 
 def main():
-    # time.sleep(1)
     clear_screen()
+    words = get_words()
+    game = Game(words)
+       
+    
+def get_words():
     args = get_args();
-    
-
-    
-
+    file_name = args.f
+    try:
+        words = []
+        with open(file_name) as file:
+            for line in file:
+                words.append(line.strip())
+        return words        
+    except FileNotFoundError:  
+        sys.exit(f"File {file_name} does not exist") 
+        
 if __name__ == '__main__':
     main()   
